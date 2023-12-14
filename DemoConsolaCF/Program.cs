3ede1205d4c2acaea01cec64fab8135afe1612ec
OrlandoYV <sergioyv@gmail.com>
2023-12-13 19:44:47 -0500

    Add project files.

---
+++ DemoConsolaCF/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Library");
+Console.WriteLine("1. Create Book");
+Console.WriteLine("2. Edit Book");
+Console.WriteLine("3. Delete Book");
+Console.WriteLine("4. Get Book");
+Console.WriteLine("5. Exit Library");
+
+
---
+++ DemoConsolaCF/utils/BookService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsolaCF.utils
+{
+    internal class BookService
+    {
+        public static string AddBook() {
+            Console.WriteLine("Add Book");
+            Console.WriteLine("Enter Title of the book");
+            string title = Console.ReadLine();
+
+            return "";
+        }
+    }
+}
